b3aa99ac3a5830ff9a1992887259a9ab6fa90d96
rrooigg <devnadifa@gmail.com>
2026-09-16 19:18:48 +0300

    calculator project setup, html & css

---
+++ calculator/calculator/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Calculator from './calculator'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Calculator />
+  )
+}
+
+export default App
---
+++ calculator/calculator/src/calculator.jsx
@@ -0,0 +1,33 @@
+function Calculator() {
+  return(
+    <div className="container">
+      <h3>Calculator</h3>
+      <div className="row">
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>x</button>
+      </div>
+      <div className="row">
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+      </div>
+      <div className="row">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+      </div>
+      <div className="row">
+        <button>0</button>
+        <button>-</button>
+        <button>=</button>
+        <button>clear</button>
+      </div>
+    </div>
+  );
+
+}
+export default Calculator
