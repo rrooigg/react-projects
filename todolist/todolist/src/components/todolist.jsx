a1b1c0413f034dcdab2e830fd60ef085f40984b2
rrooigg <devnadifa@gmail.com>
2026-09-16 11:25:43 +0300

    add functionality; display task on page, deletion

--- todolist/todolist/src/components/todolist.jsx
+++ todolist/todolist/src/components/todolist.jsx
@@ -1,14 +1,44 @@
+import { useState } from "react";
+
 function TodoList() {
+  const [ task, setTask ] = useState("");
+  const [ todolist, setTodoList ] = useState([]);
+
+  function handleSubmit() {
+    setTodoList([
+      ...todolist,
+      {task:task}
+    ]);
+    setTask("");
+  }
+  function handleDelete(index) {
+    const updatedItems = [...todolist]; 
+    updatedItems.splice(index, 1); 
+    setTodoList(updatedItems);
+    
+  }
+
   return (
     <div className="container">
       <div className="input-section">
-        <input type="text" placeholder="Enter your todo"/>
-        <button>Add</button>
+        <input 
+        type="text" 
+        placeholder="Enter your todo"
+        value={task}
+        onChange={e => setTask(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Add</button>
       </div>
-      <div className="list">
-        <p cl>Do programming assignment.</p>
-        <button>Delete</button>
-      </div>
+        <ul>
+          {todolist.map((todo, index) => (
+            <>
+             <li key={index}>
+              {todo.task}
+              <button onClick={() => handleDelete(index)}>Delete</button>
+              </li>
+            </>
+          ))}
+        </ul>
     </div>
   );
 
